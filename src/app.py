d12be4130eea512baca8a18aa569402729fab356
Brian Barquesi <barquesi.tech@gmail.com>
2022-04-21 22:12:03 -0300

    fixed app.py script, now can write in db

--- src/app.py
+++ src/app.py
@@ -4,47 +4,48 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from http import HTTPStatus as http
+from marshmallow import fields
 
 app = Flask(__name__)                                                                               ### Instanciamos a Flask en la variable app
 
-app.config['SQL_ALCHEMY_DATABASE_URI']='mysql+pymsql://root@127.0.0.1/flaskmysql'              ### Pasamos la configuración de donde está la base de datos a la app
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root@localhost/flaskmysql'                   ### Pasamos la configuración de donde está la base de datos a la app
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False                                               ### desactivamos los alerts que vienen por defecto
 
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
+db = SQLAlchemy(app)                                                                                ### Le pasamos la configuración de app al ORM SQLAlchemy para que este pueda comunicarse con la db
+ma = Marshmallow(app)                                                                               ###  instanciamos el scheme Marshmallow y le pasamos la instancia de app 
 
 
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(70), unique=True)
-    description = db.Column(db.String(100))
+class Task(db.Model):                                                                               ### Definimos la clase de tareas con los atributos id, titulo y descripción
+    id = db.Column(db.Integer, primary_key=True)                                                    ### Definimos que le id es un entero y la llave primaria
+    title= db.Column(db.String(70), unique=True)                                                    ### definimos que el titulo será un String de un max-lenght 70, y le decimos que sea unico
+    description = db.Column(db.String(255))                                                         ### Definimos que la descripción de tipo String con un max-lenght 255
+    
+    def __init__(self,title,description):                                                           ### definimos en el constructor un init que se ejecuta cada vez que se instancia la clase
+        self.title = title                                                                          ### donde definimos que el parametro title se establecera en el title
+        self.description = description                                                              ### y description se agrega al atributo description
 
-    def __init__(self, title, description):
-        self.title = title
-        self.description = description
+db.create_all()                                                                                     ### ejecutamos el metodo de SQLAlchemy create_all que lee toda la clase y crea todas las tablos
 
-db.create_all()
+class TaskSchema(ma.Schema):                                                                        ### Vamos a crear un schema que nos ayude interactuar más facilmente con la db                                                                 ### Definimos la clase Meta donde definimos los campos que vamos a querer llamar
+    id = fields.Integer(dump_only=True)
+    title = fields.String()
+    description = fields.String()
 
-class TaskSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'title', 'description')
+        
+task_Schema = TaskSchema()                                                                          ### instanciamos el Schema creado recientemente para poder llamar una tarea
+tasks_Schema = TaskSchema(many=True)                                                                ### instanciamos el Schema creado recientemente para poder llamar todas las tareas
 
+@app.route('/tasks', methods=['POST'])                                                              ### Establecemos la ruta para postear una tarea y el método HTTP con el que se va a enviar la petición
+def create_task():                                                                                  ### Definimos la función que se va a ejecutar 
+                                                                                                    ### Mostramos en la consola la respuesta que nos devuelve el servidor                                                      ### y mostramos un mensaje custom de 'received'
+    titulo = request.json['title']                                                                   ### y mostramos un mensaje custom de 'received'
+    descripcion = request.json['description']
 
-task_schema = TaskSchema()
-tasks_schema = TaskSchema(many=True)
+    new_Task = Task(titulo, descripcion)
 
-@app.route('/tasks', methods=['POST'])
-def create_task():
-  title = request.json['title']
-  description = request.json['description']
+    db.session.add(new_Task)
+    db.session.commit()
 
-  new_task= Task(title, description)
-
-  db.session.add(new_task)
-  db.session.commit()
-
-  return task_schema.jsonify(new_task)
-
-
+    return task_Schema.jsonify(new_Task)
 if __name__=='__main__':                                                                            ### Ejecutamos nuestra app con el depurador activado para poder refrescar en el momento cualquier cambio
     app.run(debug=True)
